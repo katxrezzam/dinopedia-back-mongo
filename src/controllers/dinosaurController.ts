0df3ba540c8e272aeafff6a2bdceac463e114625
Adrian James <katxrezzam@hotmail.com>
2023-03-09 16:48:30 -0500

    fixing formating naming for blobs

--- src/controllers/dinosaurController.ts
+++ src/controllers/dinosaurController.ts
@@ -2,11 +2,12 @@ import { RequestHandler } from "express";
 import { create, destroy, findAll, findById, update } from "../services/dinosaurServices";
 import { errorMessage, errorJson } from "../util/errorLogger";
 import { toNewDinosaur, toNewUpdateDinosaur } from "../util/validation/dinosaurValidation";
-import { bufferValidation, idSchemaValidation, stringValidation } from "../util/validation/_validation";
+import { bufferValidation, idSchemaValidation } from "../util/validation/_validation";
 import { UpdateDinosaur } from '../types'
 import { uploadBlob } from "../services/blobServices";
 
 const container = 'dinosaurs'
+const BLOB_URI = 'https://dinopediablob.blob.core.windows.net/dinosaurs/'
 
 export const findAllDinosaur: RequestHandler = async (_req, res) => {
   try {
@@ -24,13 +25,22 @@ export const createDinosaur: RequestHandler = async (req, res) => {
 
     const { buffer, originalname } = req.file as Express.Multer.File
 
+    const wordsplited = originalname.split('.')
+
+    const fileExtension = wordsplited[1]
+
     const parsedBuffer = bufferValidation(buffer)
 
-    const parsedFileName = stringValidation(originalname)
-
-    await uploadBlob(parsedBuffer, parsedFileName, container)
-
     const parsedDino = await create(newDino)
+
+    const fileName = `${parsedDino._id}.${fileExtension}`
+
+    await uploadBlob(parsedBuffer, fileName, container)
+
+    parsedDino.url_image = BLOB_URI+fileName
+    parsedDino.url_render = BLOB_URI+fileName
+
+    await update(parsedDino._id, parsedDino)
     
     res.json(parsedDino)
   } catch(error){
